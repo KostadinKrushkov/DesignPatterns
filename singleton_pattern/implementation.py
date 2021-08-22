7b1794c4e01ffbe7c062ad808b23b87823271edc
Kostadin <k.krushkov98@gmail.com>
2021-08-22 23:26:55 +0300

    Singleton design pattern

---
+++ singleton_pattern/implementation.py
@@ -0,0 +1,37 @@
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        # else:  # If you want to run the __init__ method of a singleton when you call it
+        #     cls._instances[cls].__init__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class FileProxySingleton(metaclass=SingletonMeta):
+    default_filename = 'testing.txt'
+
+    def write_to_file(self, data, filename=default_filename):
+        with open(filename, 'w') as f:
+            f.write(data)
+        print('Data saved')
+
+    def append_to_file(self, data, filename=default_filename):
+        with open(filename, 'a') as f:
+            f.write(data)
+        print('Data added')
+
+    def read_from_file(self, filename=default_filename):
+        with open(filename, 'r') as f:
+            read_data = f.read()
+
+        return read_data
+
+
+if __name__ == '__main__':
+    file_proxy = FileProxySingleton()
+    file_proxy.write_to_file('Test writing some data\n')
+    file_proxy.append_to_file('Test writing some other data\n')
+
+    print(file_proxy.read_from_file())
